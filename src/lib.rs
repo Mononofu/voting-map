0585177f96b8e3be25fda48b10213afca1d7c26e
Julian Schrittwieser <j.schrittwieser@gmail.com>
2020-07-24 22:44:11 +0100

    support more voting systems and add example candidate distributions

--- src/lib.rs
+++ src/lib.rs
@@ -79,11 +79,7 @@ fn normal_pdf(mean: f32, sigma: f32, x: f32) -> f32 {
         * (-1f32 / 2f32 * ((x - mean) / sigma).powi(2)).exp()
 }
 
-fn vote_dummy(_p: Point, _candidates: &[Point]) -> u8 {
-    0
-}
-
-fn vote_plurality(p: Point, candidates: &[Point]) -> u8 {
+fn vote_plurality(p: Point, candidates: &[Point], votes: &mut [u8]) {
     let mut closest_i = 100000000;
     let mut closest_dist = std::f32::MAX;
     for (i, c) in candidates.iter().enumerate() {
@@ -93,7 +89,53 @@ fn vote_plurality(p: Point, candidates: &[Point]) -> u8 {
             closest_i = i;
         }
     }
-    closest_i as u8
+    votes[closest_i] = 1;
+}
+
+fn vote_close(p: Point, candidates: &[Point], votes: &mut [u8]) {
+    for (i, c) in candidates.iter().enumerate() {
+        let dist = p.l2_square(c);
+        if dist < 1.0 {
+            votes[i] = 1;
+        }
+    }
+}
+
+fn vote_rank(p: Point, candidates: &[Point], votes: &mut [u8]) {
+    let mut min_dist = std::f32::MIN;
+    let mut prev_i = 1000000;
+    for rank in 0..candidates.len() {
+        let mut closest_i = 100000000;
+        let mut closest_dist = std::f32::MAX;
+        for (i, c) in candidates.iter().enumerate() {
+            let dist = p.l2_square(c);
+            if dist < closest_dist && dist >= min_dist && i != prev_i {
+                closest_dist = dist;
+                closest_i = i;
+            }
+        }
+        min_dist = closest_dist;
+        prev_i = closest_i;
+        votes[closest_i] = (1 + rank) as u8;
+    }
+}
+
+fn max_vote_candidate(votes: &[f32]) -> usize {
+    votes
+        .iter()
+        .enumerate()
+        .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
+        .map(|(i, _)| i)
+        .unwrap()
+}
+
+fn min_vote_candidate(votes: &[f32]) -> usize {
+    votes
+        .iter()
+        .enumerate()
+        .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
+        .map(|(i, _)| i)
+        .unwrap()
 }
 
 const CANDIDATE_COLORS: [Color; 5] = [
@@ -133,6 +175,12 @@ pub fn max_candidates() -> usize {
     CANDIDATE_COLORS.len()
 }
 
+#[wasm_bindgen]
+pub fn candidate_color(i: usize) -> String {
+    let color = CANDIDATE_COLORS[i];
+    format!("rgb({}, {}, {})", color.r, color.g, color.b)
+}
+
 fn compute_votes<F>(
     size: i32,
     start: i32,
@@ -141,28 +189,53 @@ fn compute_votes<F>(
     voting_method: F,
 ) -> Vec<u8>
 where
-    F: Fn(Point, &[Point]) -> u8,
+    F: Fn(Point, &[Point], &mut [u8]),
 {
     let padded_size = (end - start) as i32;
-    let mut results = vec![0u8; padded_size.pow(2) as usize];
+    let mut results = vec![0u8; padded_size.pow(2) as usize * candidates.len()];
 
     // Compute voting results at each individual point.
     for x in start..end {
         for y in start..end {
             let at = Point::new(x as f32 / size as f32, y as f32 / size as f32);
-            let winner = voting_method(at, &candidates);
 
             let i = x - start;
             let j = y - start;
-            results[(i * padded_size + j) as usize] = winner;
+            let offset = ((i * padded_size + j) as usize) * candidates.len();
+
+            voting_method(
+                at,
+                &candidates,
+                &mut results[offset..offset + candidates.len()],
+            );
         }
     }
 
     results
 }
 
+fn declare_winner<F>(
+    size: i32,
+    num_votes: &[f32],
+    candidates: &[Point],
+    select_winner: F,
+) -> Vec<u8>
+where
+    F: Fn(&[f32]) -> usize,
+{
+    let mut winners = vec![0u8; size.pow(2) as usize];
+    for x in 0..size {
+        for y in 0..size {
+            let i = ((x * size) + y) as usize * candidates.len();
+            let winner = select_winner(&num_votes[i..i + candidates.len()]);
+            winners[(x * size + y) as usize] = winner as u8;
+        }
+    }
+    winners
+}
+
 pub fn election(size: i32, candidates: &[Point], election_method: &str) -> Vec<u8> {
-    let sigma = 0.5f32;
+    let sigma = 0.5f32 / 1.5;
     let num_sigma = 3.0;
 
     // The vote map is [0; 1], so we need to compute votes in [-num_sigma * sigma; 1 + num_sigma * sigma].
@@ -173,7 +246,9 @@ pub fn election(size: i32, candidates: &[Point], election_method: &str) -> Vec<u
     // Compute voting results at each individual point.
     let results = match election_method {
         "plurality" => compute_votes(size, start, end, &candidates, vote_plurality),
-        _ => compute_votes(size, start, end, &candidates, vote_dummy),
+        "approval" => compute_votes(size, start, end, &candidates, vote_close),
+        "borda" | "condorcet" | "hare" => compute_votes(size, start, end, &candidates, vote_rank),
+        _ => unreachable!("unsupported election method {}", election_method),
     };
 
     // Neighbourhood weighting.
@@ -193,8 +268,10 @@ pub fn election(size: i32, candidates: &[Point], election_method: &str) -> Vec<u
             for (dx, p) in sample_locations.iter() {
                 let i = x + dx - start;
                 let j = y - start;
-                let winner = results[(i * padded_size + j) as usize];
-                line_votes[winner as usize] += p;
+                let offset = ((i * padded_size + j) as usize) * candidates.len();
+                for c in 0..candidates.len() {
+                    line_votes[c] += results[offset + c] as f32 * p;
+                }
             }
 
             // Add the summed votes to all points with the same x coordinate,
@@ -212,21 +289,15 @@ pub fn election(size: i32, candidates: &[Point], election_method: &str) -> Vec<u
     }
 
     // Select the winner of the election for each point.
-    let mut winners = vec![0u8; size.pow(2) as usize];
-    for x in 0..size {
-        for y in 0..size {
-            let i = ((x * size) + y) as usize * candidates.len();
-            let votes = &num_votes[i..i + candidates.len()];
-
-            let winner = votes
-                .iter()
-                .enumerate()
-                .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
-                .map(|(i, _)| i)
-                .unwrap();
-            winners[(x * size + y) as usize] = winner as u8;
+    match election_method {
+        "plurality" | "approval" => {
+            declare_winner(size, &num_votes, candidates, max_vote_candidate)
         }
+        "borda" => declare_winner(size, &num_votes, candidates, min_vote_candidate),
+        "condorcet" => unimplemented!(
+            "condorcet; must select candidate that would defeat all others one-to-one"
+        ),
+        "hare" => unimplemented!("hare"),
+        _ => unreachable!("unsupported election method {}", election_method),
     }
-
-    winners
 }
