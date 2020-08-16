6fa1783d5dc97e9cec9be85b5b81db51e2aef45d
Julian Schrittwieser <j.schrittwieser@gmail.com>
2020-08-16 22:02:54 +0100

    make benchmarks run again

--- benches/vote.rs
+++ benches/vote.rs
@@ -10,9 +10,9 @@ fn criterion_benchmark(c: &mut Criterion) {
     ];
 
     let mut group = c.benchmark_group("election");
-    for size in [64, 128, 256].iter() {
+    for size in [32].iter() {
         group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
-            b.iter(|| election(size, &candidates))
+            b.iter(|| election(size, &candidates, "hare"))
         });
     }
 }
